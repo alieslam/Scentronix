201b2e05944c6070908ad63e3447473526d6c743
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2023-04-03 11:38:39 +0200

    Added theme color by system but commented it for now for flickering problem

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,21 @@
-import Header from '@/components/Header'
-import './globals.css'
+import Header from "@/components/Header";
+import "./globals.css";
+import Providers from "./Providers";
 
 export const metadata = {
-  title: 'Algorithmic Perfumery',
-  description: 'By Scentronix',
-}
+  title: "Algorithmic Perfumery",
+  description: "By Scentronix",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {/*Header*/}
-        <Header/>
-        {/*NavBar*/}
-        {children}</body>
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,20 @@ import { AiFillShop } from "react-icons/ai";
 import { BiFoodMenu } from "react-icons/bi";
 import { AiOutlineRead } from "react-icons/ai";
 import { AiFillInfoCircle } from "react-icons/ai";
+import DarkModeSwitch from "./DarkModeSwitch";
 
 export default function Header() {
   return (
+    <div className="flex">
       <div className="flex justify-center my-6 sm:mx-auto">
         <MenuItem title="SHOP" address="/" Icon={AiFillShop} />
         <MenuItem title="RECIPES" address="/recipes" Icon={BiFoodMenu} />
         <MenuItem title="BLOG" address="/blog" Icon={AiOutlineRead} />
         <MenuItem title="ABOUT" address="/about" Icon={AiFillInfoCircle} />
       </div>
+      <div className="flex items-center justify-center px-4">
+        {/* <DarkModeSwitch/> */}
+      </div>
+    </div>
   );
 }
