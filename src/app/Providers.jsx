f3ea1ace1de96c3b397716532ff4ed6df4bdd2e0
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2023-04-03 11:58:24 +0200

    Pretifying code

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -1,10 +1,10 @@
-'use client'
-import { ThemeProvider } from 'next-themes'
+"use client";
+import { ThemeProvider } from "next-themes";
 
-export default function Providers({children}) {
+export default function Providers({ children }) {
   return (
-    <ThemeProvider enableSystem={true} attribute='class'>
-        {children}
+    <ThemeProvider enableSystem={true} attribute="class">
+      <div>{children}</div>
     </ThemeProvider>
-  )
+  );
 }
