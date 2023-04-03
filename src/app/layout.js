00f66f8ddb97d02181612c42ccf426bcf4ce38c6
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2023-04-03 10:42:19 +0200

    Fixed typo in header

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import Header from '@/components/Header'
 import './globals.css'
 
 export const metadata = {
-  title: 'Algorithimic Perfumery',
+  title: 'Algorithmic Perfumery',
   description: 'By Scentronix',
 }
 
