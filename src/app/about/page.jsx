1cdf4ac49ca98bef959e089337c25e54f581239e
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2023-04-04 11:29:04 +0200

    Added some data in about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,27 +4,48 @@ import healthyFoodPicture from "../../../public/healthyFood.png";
 
 export default function About() {
   return (
-    <div className="max-w-6xl  mx-6 space-y-4 space-x-4">
-      <h1 className="text-1xl font-medium">About</h1>
-      <div className="flex items-center justify-center">
-      <div className="text-center">
-        <Image width={500} height={500} alt="This is a food for pic" src={healthyFoodPicture} />
+    <div className="max-w-full  my-16 space-y-16 px-8">
+      <div className="flex  justify-evenly">
+        <div className="flex justify-start items-start">
+          <Image
+            width={600}
+            height={600}
+            alt="This is a food for pic"
+            src={healthyFoodPicture}
+          />
+        </div>
+        <div className="max-w-lg space-y-8">
+          <h1>Empowering people by harnessing their likings.</h1>
+          <p>
+            At our company, we are passionate about providing delicious and
+            nutritious food recipes to our customers. Our team of experienced
+            chefs and nutritionists work together to create recipes that not
+            only taste great but also promote a healthy lifestyle. We understand
+            that people have different dietary needs and preferences, which is
+            why we offer a wide range of recipes that cater to different dietary
+            requirements such as vegan, gluten-free, and keto. Our recipes are
+            easy to follow and come with step-by-step instructions and
+            nutritional information to help our customers make informed choices.
+            We believe that good food should not only taste good but also make
+            you feel good, and that's what we strive to achieve with every
+            recipe we create.
+          </p>
+        </div>
       </div>
+      <div className="flex justify-start pl-24">
+        <div className="max-w-lg space-y-8">
+          <h1>
+            We believe every human is a creator with the birthright to make
+            choices freely
+          </h1>
+          <p>
+            The joy of creation connects us to ourselves, to others and the
+            world around us. This connection unleashes our imagination to dream,
+            to play, to create. We believe imagination is the everlasting magic
+            behind progress and happiness.
+          </p>
+        </div>
       </div>
-      <p>
-        At our company, we are passionate about providing delicious and
-        nutritious food recipes to our customers. Our team of experienced chefs
-        and nutritionists work together to create recipes that not only taste
-        great but also promote a healthy lifestyle. We understand that people
-        have different dietary needs and preferences, which is why we offer a
-        wide range of recipes that cater to different dietary requirements such
-        as vegan, gluten-free, and keto. Our recipes are easy to follow and come
-        with step-by-step instructions and nutritional information to help our
-        customers make informed choices. We believe that good food should not
-        only taste good but also make you feel good, and that's what we strive
-        to achieve with every recipe we create.
-      </p>
-      <div></div>
     </div>
   );
 }
