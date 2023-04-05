8e6cfcc56052753fc1a53b7db524dff4d6fe1a00
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2023-04-05 14:19:03 +0200

    Switched from CardMedia to Image and did some styling

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -2,12 +2,13 @@
 import React from "react";
 import { Card, CardMedia, CardContent, Typography } from "@mui/material";
 import CustomTypography from "./CustomTypography";
+import Image from "next/image";
 
 const styles = {
   card: {
     display: "flex",
     marginBottom: 16,
-    // borderRadius: 16,
+    borderRadius: 0,
     // boxShadow:
     //   "0px 8px 10px -5px rgba(0,0,0,0.2), 0px 16px 24px 2px rgba(0,0,0,0.14), 0px 6px 30px 5px rgba(0,0,0,0.12)",
     minHeight: 400,
@@ -16,11 +17,7 @@ const styles = {
     flexWrap: "wrap",
   },
   media: {
-    minWidth: 400,
-    maxWidth: 400,
-    minHeight: 400,
-    maxHeight: 400,
-    borderRadius: "16px 16px 16px 16px",
+    objectFit:'contain',
   },
   content: {
     flex: "1 0",
@@ -32,20 +29,22 @@ const styles = {
 export default function BlogCard({ blogData }) {
   return (
     <Card style={styles.card} elevation={0}>
-      <CardMedia
-        style={styles.media}
-        image={blogData.thumbnail}
-        title={blogData.thumbnail}
-      />
+      {/* Switched from CardMedia to Image since Image handles the responsiveness */}
+      <div className="flex justify-start items-start">
+        <Image
+          width={400}
+          height={400}
+          alt={""}
+          src={blogData.thumbnail}
+          style={styles.media}
+          onError={(event) => event.target.removeAttribute("src")}
+        />
+      </div>
       <CardContent style={styles.content}>
-        <CustomTypography gutterBottom variant="h5" component="h2">
+        <CustomTypography variant="h5" component="h2">
           {blogData.blogName}
         </CustomTypography>
-        <CustomTypography
-          variant="body2"
-          color="textSecondary"
-          noWrap={false}
-        >
+        <CustomTypography variant="body2" color="textSecondary" noWrap={false}>
           {blogData.blogText}
         </CustomTypography>
       </CardContent>
