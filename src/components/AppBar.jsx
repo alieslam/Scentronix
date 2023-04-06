ad90dddd1f955faceee6787a5e102d2a2d73eb2f
Eslam Ali <eslam.mohamed.fathy@gmail.com>
2023-04-06 11:47:37 +0200

    Add App bar scroll animation

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,12 +1,28 @@
 "use client";
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
+import { Slide } from "@mui/material";
+import useScrollTrigger from '@mui/material/useScrollTrigger';
 
-function ResponsiveAppBar({children, appBarStyle, containerStyle}) {
+function HideOnScroll(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
   return (
-    <AppBar {...appBarStyle}>
-        {children}
-    </AppBar>
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+function ResponsiveAppBar(props) {
+  const { children, appBarStyle, containerStyle } = props
+  return (
+    <HideOnScroll {...props}>
+      <AppBar {...appBarStyle}>{children}</AppBar>
+    </HideOnScroll>
   );
 }
 export default ResponsiveAppBar;
