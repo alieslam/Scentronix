6cfdf4e996c95f1b27bba29558314fd08c188ca7
alieslam <eslam.ali@elkrem.io>
2023-04-09 14:03:23 +0200

    Support shop Item details and added tags for seos

--- src/app/[itemId]/page.jsx
+++ src/app/[itemId]/page.jsx
@@ -1,5 +1,28 @@
+import ShopItemDetails from "@/components/ShopItemDetails";
 import React from "react";
 
-export default function ShopItem() {
-  return <div>ShopItem</div>;
+async function getItemDetails(itemId) {
+  const res = await require("../../../public/shopData.json"); // TODO: replace this dummy call with a regular fetching query
+  const item = {
+    ...res.find((itm) => itm.uid === itemId),
+  };
+  res.ok = true; // TODO: remove this line as it is just added for passing the response status check
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return item;
 }
+
+export async function generateMetadata({ params }) {
+  const product = await getItemDetails(params.itemId);
+  return { title: product.name, description: product.name }
+}
+
+export default async  function ShopItem(props) {
+  const {params} = props;
+  const shopItem =await  getItemDetails(params.itemId);
+  return (
+   <ShopItemDetails item={shopItem}/>
+  );
+}
+
