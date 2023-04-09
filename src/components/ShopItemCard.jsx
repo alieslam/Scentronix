fc65164b32ac69463dce5161bc12b61197f51a9e
alieslam <eslam.ali@elkrem.io>
2023-04-09 10:03:05 +0200

    Added more data to shopitem card

--- src/components/ShopItemCard.jsx
+++ src/components/ShopItemCard.jsx
@@ -2,25 +2,33 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import CustomTypography from "./CustomTypography";
+import StarRating from "./StarRating";
 
 export default function ShopItemCard({ shopItem }) {
   return (
     <Link href={`/${shopItem.uid}`}>
-      <div className="rounded-lg flex flex-row md:flex-col cursor-pointer md:max-w-xs max-w-full w-full bg-custom-brown bg-opacity-20 bg-blur-sm backdrop-filter backdrop-blur-sm">
+      <div className="rounded-sm flex flex-row md:flex-col cursor-pointer md:max-w-xs max-w-full w-full bg-custom-brown bg-opacity-20 bg-blur-sm backdrop-filter backdrop-blur-sm">
         <div className="w-1/3 md:w-full">
           <Image
             src={shopItem.image}
             width={500}
             height={500}
-            className="object-cover h-40 rounded-lg"
+            className="object-cover h-60 rounded-sm"
             placeholder="blur"
             blurDataURL="/spinner.svg"
             alt="image is not available"
           ></Image>
         </div>
         <div className="p-4 w-2/3 md:w-full">
-          <h2 className="truncate text-lg font-bold">{shopItem.name}</h2>
-          <p className="line-clamp-4 flex-wrap">descriptionasdkajsdhjashdkjasdasdasdasdasdasdjs</p>
+          <CustomTypography className="truncate font-bold" variant="h2" component="h2">{shopItem.name}</CustomTypography>
+          <StarRating value={shopItem.ratings}/>
+          <CustomTypography className="text-gray-700 text-base h-30 leading-relaxed py-2 overflow-hidden flex-shrink" variant="body2" color="textSecondary" component="p">
+           {shopItem.description}
+          </CustomTypography>
+          <CustomTypography className="text-right" variant="h2" color="textSecondary" component="h2">
+           {shopItem.price.toFixed(2)} €
+          </CustomTypography>
         </div>
       </div>
     </Link>
---
+++ src/components/StarRating.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Rating } from '@mui/material';
+
+
+export default function StarRating({ value }) {
+  return (
+    <div className="flex items-center">
+      <Rating name="read-only" value={value} readOnly />
+      <span>{value}</span>
+    </div>
+  );
+}
