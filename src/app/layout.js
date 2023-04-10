7c84bef04c1743b2639c6684760090109d2c8c0d
alieslam <eslam.ali@elkrem.io>
2023-04-10 12:57:21 +0200

    Extract route name from layout to configure header parts dynamically

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-import Header from "@/components/Header";
 import "./globals.css";
 import Providers from "./Providers";
 import ResponsiveAppBar from "@/components/AppBar";
@@ -9,7 +8,8 @@ export const metadata = {
   description: "By Scentronix",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, params }) {
+  const route = children.props.childProp.segment; // There seems to be an issue not passing params and searchParams to root layout https://github.com/vercel/next.js/issues/43704
   return (
     <html lang="en">
       <body>
@@ -17,14 +17,15 @@ export default function RootLayout({ children }) {
           <ResponsiveAppBar
             appBarStyle={{
               position: "sticky",
-              elevation: 2,
+              elevation: 0,
               sx: {
                 mb: 1,
                 height: 100,
-                boxShadow:
-                  "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)",
+                // boxShadow:
+                //   "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)",
               },
             }}
+            route={route}
           >
             <MainAppBarItems />
           </ResponsiveAppBar>
