d0ace494b15be755b577c97f7f4e26927fb84b37
alieslam <eslam.ali@elkrem.io>
2023-04-10 12:55:30 +0200

    Added scroll up/down animation instead of Sliding

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,28 +1,27 @@
 "use client";
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import { Slide } from "@mui/material";
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-
-function HideOnScroll(props) {
-  const { children, window } = props;
-  const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
-  });
-
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
+import { Collapse, Slide } from "@mui/material";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import RecipesSubBar from "./RecipesSubBar";
+import { useScrollDirection } from "@/hooks/scrollHook";
 
 function ResponsiveAppBar(props) {
-  const { children, appBarStyle, containerStyle } = props
+  const { children, appBarStyle, route } = props;
+  const scrollDirection = useScrollDirection();
   return (
-    <HideOnScroll {...props}>
-      <AppBar {...appBarStyle}>{children}</AppBar>
-    </HideOnScroll>
+    <>
+      {/* <HideOnScroll {...props}> */}
+      <div
+        className={`sticky ${
+          scrollDirection === "down" ? "-top-28" : "top-0"
+        } h-28 transition-all duration-500 z-10`}
+      >
+        <AppBar {...appBarStyle}>{children}</AppBar>
+        {route === "recipes" && <RecipesSubBar />}
+      </div>
+      {/* </HideOnScroll> */}
+    </>
   );
 }
 export default ResponsiveAppBar;
