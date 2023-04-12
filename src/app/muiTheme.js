7566de171de8129d10fc4a6335bb205608ed8944
alieslam <eslam.ali@elkrem.io>
2023-04-12 09:50:48 +0200

    Added custom color to MuiTheme

--- src/app/muiTheme.js
+++ src/app/muiTheme.js
@@ -12,6 +12,12 @@ const theme = createTheme({
     secondary: {
       main: "#8d6e63",
     },
+    custom: {
+      light: "#ffa726",
+      main: "#f57c00",
+      dark: "#ef6c00",
+      contrastText: "rgba(0, 0, 0, 0.9)",
+    },
   },
   typography: {
     fontFamily: "Roboto, sans-serif",
