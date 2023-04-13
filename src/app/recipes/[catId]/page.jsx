14e58dd49c236e0be7a5130c28ae27f830aab7a8
alieslam <eslam.ali@elkrem.io>
2023-04-13 11:41:47 +0200

    Added not found page

--- src/app/recipes/[catId]/page.jsx
+++ src/app/recipes/[catId]/page.jsx
@@ -1,9 +1,10 @@
 import RecipesGrid from "@/components/RecipesGrid";
+import { notFound } from "next/navigation";
 import React from "react";
 
 async function getRecipes(recipeId) {
   const res = await fetch("http://localhost:3000/recipes.json", {
-    cache: 'no-store'
+    cache: "no-store",
   });
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -18,5 +19,8 @@ async function getRecipes(recipeId) {
 export default async function RecipesList(props) {
   const { params } = props;
   const recipes = await getRecipes(params.catId);
+  if (!recipes) {
+    notFound();
+  }
   return <RecipesGrid items={recipes} categoryId={params.catId} />;
 }
